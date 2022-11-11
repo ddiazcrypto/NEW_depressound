27ec8809a16907d359f982d281147f811f3075fd
BrigitteMendez1302 <U201914955@upc.edu.pe>
2022-11-11 00:11:37 -0500

    comment some lines

--- Build-a-User-Authentication-Web-App-With-Python-and-Django-master/authenticate/views.py
+++ Build-a-User-Authentication-Web-App-With-Python-and-Django-master/authenticate/views.py
@@ -32,15 +32,15 @@ def home(request):
 
 
 def home_login(request):
-    a1 = Formulario_X_Pregunta.objects.select_related('Resultado_Resultado_Codigo')
-    print(str(a1.query))
+    # a1 = Formulario_X_Pregunta.objects.select_related('Resultado_Resultado_Codigo')
+    # print(str(a1.query))
 
-    k=Formulario_X_Pregunta.objects.filter(Formulario_Formulario_Codigo=8).select_related('Resultado_Resultado_Codigo')
+    # k=Formulario_X_Pregunta.objects.filter(Formulario_Formulario_Codigo=8).select_related('Resultado_Resultado_Codigo')
 
-    results = Resultado.objects()
-    print('so k ', k)
-    print('results ', results)
-    print('BRII 3', str(k.query))
+    # results = Resultado.objects()
+    # print('so k ', k)
+    # print('results ', results)
+    # print('BRII 3', str(k.query))
 
     return render(request, 'authenticate/home_login.html', {})
 
